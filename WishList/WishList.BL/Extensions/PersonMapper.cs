e88607a8b42b527d46e8f9d0f150ddab8f9be3bf
Jules Verstraeten <jules.verstraeten@student.hogent.be>
2025-12-22 10:58:23 +0100

    Just a ave

--- WishList/WishList.BL/Extensions/PersonMapper.cs
+++ WishList/WishList.BL/Extensions/PersonMapper.cs
@@ -1,8 +1,17 @@
 ﻿using WishList.BL.Models;
+using WishList.DL.Entities;
 
 namespace WishList.BL.Extensions;
 
 public static class PersonMapper
 {
-    
+    public PersonEntity AsEntity(PersonModel model)
+    {
+        return new PersonEntity
+        {
+            Id = model.Id ?? null,
+            FirstName = model.FirstName,
+            LastName = model.LastName,
+        };
+    }
 }
--- WishList/WishList.BL/Interfaces/IPersonService.cs
+++ WishList/WishList.BL/Interfaces/IPersonService.cs
@@ -1,6 +1,10 @@
-﻿namespace WishList.BL.Interfaces;
+﻿using WishList.BL.Models;
+
+namespace WishList.BL.Interfaces;
 
 public interface IPersonService
 {
-    
+    Task<List<PersonModel>> GetAllPersonsAsync();
+    Task<List<PersonModel>> GetPersonByIdAsync(int personId);
+    Task<PersonModel> SavePersonAsync(PersonModel person);
 }
